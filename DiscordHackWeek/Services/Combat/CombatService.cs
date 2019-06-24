3268ab2fd60707ddebad368d0cf4ad9de31804b2
Sphexator <kristoffer.tunlid@gmail.com>
2019-06-24 22:31:16 +0200

    Added combat service

---
+++ DiscordHackWeek/Services/Combat/CombatService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DiscordHackWeek.Services.Combat
+{
+    public class CombatService
+    {
+        public readonly MemoryCache Consumables 
+            = new MemoryCache(new MemoryCacheOptions
+                { ExpirationScanFrequency = TimeSpan.FromMinutes(1) });
+
+        public async Task Enemy()
+        {
+
+        }
+
+        public async Task Battle()
+        {
+
+        }
+
+        public int Damage()
+        {
+
+            return 0;
+        }
+    }
+}
