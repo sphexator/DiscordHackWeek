ca34d9747c0019e340450628caa92af431e4c36f
Sphexator <kristoffer.tunlid@gmail.com>
2019-06-24 22:48:37 +0200

    Added monster type and db table

---
+++ DiscordHackWeek/Entities/EnemyType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordHackWeek.Entities
+{
+    public enum EnemyType
+    {
+        Beast,
+        Humanoid,
+        Demon
+    }
+}
---
+++ DiscordHackWeek/Services/Database/Tables/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using DiscordHackWeek.Entities;
+
+namespace DiscordHackWeek.Services.Database.Tables
+{
+    public class Enemy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public EnemyType Type { get; set; }
+        public int Level { get; set; }
+        public string LightAttack { get; set; }
+        public string HeavyAttack { get; set; }
+    }
+}
