3e0a84494ceb7cad154db771c1e6d554551c5072
Sphexator <kristoffer.tunlid@gmail.com>
2019-06-25 10:42:32 +0200

    renamed combat service

--- DiscordHackWeek/Services/Combat/CombatHandling.cs
+++ DiscordHackWeek/Services/Combat/CombatHandling.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
+using DiscordHackWeek.Entities;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace DiscordHackWeek.Services.Combat
 {
-    public class CombatService
+    public class CombatHandling : INService
     {
         public readonly MemoryCache Consumables 
             = new MemoryCache(new MemoryCacheOptions
