f9c595f679dbbedc953fdc9015a307f7e5500e2a
Sphexator <kristoffer.tunlid@gmail.com>
2019-06-25 12:42:40 +0200

    Calculate damage and naming changes

--- DiscordHackWeek/Services/Combat/CombatHandling.cs
+++ DiscordHackWeek/Services/Combat/CombatHandling.cs
@@ -14,17 +14,37 @@ namespace DiscordHackWeek.Services.Combat
 
         public readonly MemoryCache Consumables
             = new MemoryCache(new MemoryCacheOptions
-                {ExpirationScanFrequency = TimeSpan.FromMinutes(1)});
+                { ExpirationScanFrequency = TimeSpan.FromMinutes(1) });
 
         public CombatHandling(Random random) => _random = random;
 
-        public async Task Battle(User userData, Enemy enemyData, DbService db)
+        public async Task BattleAsync(User userData, Enemy enemyData, DbService db)
         {
-            var user = BuildCombatUser(userData, db);
-            var enemy = BuildCombatUser(enemyData, db);
+            var user = BuildCombatUserAsync(userData, db);
+            var enemy = BuildCombatUserAsync(enemyData, db);
+            var battle = true;
+            while (battle)
+            {
+
+                battle = false;
+            }
         }
 
-        private async Task<CombatUser> BuildCombatUser(User userData, DbService db)
+        private int CalculateDamage(CombatUser user)
+        {
+            var critChance = _random.Next(100);
+            var dmg = 10 * user.AttackPower;
+            if (critChance >= user.CriticalChance) dmg = Convert.ToInt32(dmg * 1.5);
+
+            var lowDmg = dmg / 1.5;
+            if (lowDmg <= 0) lowDmg = 5;
+            var highDmg = dmg * 1.5;
+            if (lowDmg >= highDmg) highDmg = lowDmg + 10;
+
+            return _random.Next(Convert.ToInt32(lowDmg), Convert.ToInt32(highDmg));
+        }
+
+        private async Task<CombatUser> BuildCombatUserAsync(User userData, DbService db)
         {
             var weapon = await db.Items.FindAsync(userData.WeaponId);
             var armor = await db.Items.FindAsync(userData.ArmorId);
@@ -33,46 +53,42 @@ namespace DiscordHackWeek.Services.Combat
                 Health = 10 * userData.Level + 10 * userData.HealthTalent + 10 * armor.HealthIncrease,
                 AttackPower = 1 * userData.Level + 10 * userData.DamageTalent + 10 * weapon.DamageIncrease +
                               10 * armor.DamageIncrease,
-                CriticalChance = 1 + 10 * weapon.CritIncrease + 10 * armor.CritIncrease,
+                CriticalChance = 15 + 1 * weapon.CritIncrease + 10 * armor.CritIncrease,
                 DmgTaken = 0
             };
             if (Consumables.TryGetValue(userData.UserId, out var item) && item is Item consum)
             {
                 user.AttackPower += 10 * consum.DamageIncrease;
-                user.CriticalChance += 10 * consum.CritIncrease;
+                user.CriticalChance += 1 * consum.CritIncrease;
                 user.Health += 10 * consum.HealthIncrease;
             }
 
             return user;
         }
 
-        private async Task<CombatUser> BuildCombatUser(Enemy enemyData, DbService db)
+        private async Task<CombatUser> BuildCombatUserAsync(Enemy enemyData, DbService db)
         {
             var enemy = new CombatUser
             {
                 Health = 15 * enemyData.Level,
                 AttackPower = 1 * enemyData.Level,
-                DmgTaken = 0
+                DmgTaken = 0,
+                CriticalChance = 10
             };
             if (enemyData.ArmorId.HasValue)
             {
                 var eArmor = await db.Items.FindAsync(enemyData.ArmorId);
                 enemy.Health += 10 * eArmor.HealthIncrease;
-                enemy.CriticalChance += 10 * eArmor.CritIncrease;
+                enemy.CriticalChance += 1 * eArmor.CritIncrease;
                 enemy.AttackPower += 10 * eArmor.DamageIncrease;
             }
 
             if (!enemyData.WeaponId.HasValue) return enemy;
             var eWeapon = await db.Items.FindAsync(enemyData.WeaponId);
             enemy.AttackPower += 10 * eWeapon.DamageIncrease;
-            enemy.CriticalChance += 10 * eWeapon.CritIncrease;
+            enemy.CriticalChance += 1 * eWeapon.CritIncrease;
 
             return enemy;
-        }
-
-        private int CalculateDamage(CombatUser user)
-        {
-            return 0;
         }
     }
 }
