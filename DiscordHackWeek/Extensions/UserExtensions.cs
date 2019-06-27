81827c4b7c8a4f1235c4be63f2df9e3547da4121
Sphexator <kristoffer.tunlid@gmail.com>
2019-06-27 16:49:49 +0200

    Added embed & user extensions

---
+++ DiscordHackWeek/Extensions/UserExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+using Discord.WebSocket;
+
+namespace DiscordHackWeek.Extensions
+{
+    public static class UserExtensions
+    {
+        public static string GetAvatar(this SocketUser user)
+        {
+            var avi = user.GetAvatarUrl(ImageFormat.Auto, 2048);
+            if (avi != null) return avi;
+            return user.GetDefaultAvatarUrl();
+        }
+    }
+}
