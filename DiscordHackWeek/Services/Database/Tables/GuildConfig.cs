0b7b5934d696d2abd6722af927b850169fc000ab
Sphexator <kristoffer.tunlid@gmail.com>
2019-06-28 09:20:00 +0200

    Added db configs

---
+++ DiscordHackWeek/Services/Database/Tables/GuildConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DiscordHackWeek.Services.Database.Tables
+{
+    public class GuildConfig
+    {
+        public ulong GuildId { get; set; }
+        public List<string> Prefixes { get; set; }
+    }
+}
---
+++ DiscordHackWeek/Services/Database/Tables/IgnoreChannel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DiscordHackWeek.Services.Database.Tables
+{
+    public class IgnoreChannel
+    {
+        public ulong GuildId { get; set; }
+        public List<ulong> IgnoreList { get; set; }
+    }
+}
